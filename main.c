cd6ecc303b26f4aa18d7a8cfd501b3a4ec44b23c
p156 <38420559+p156@users.noreply.github.com>
2018-10-16 13:30:28 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	char str[7];
+	char array[100];
+	int index = 0;
+	int j = 0;
 
-    return 0;
+	while (fgets(str, sizeof(str), stdin)) {
+
+		printf("%s", str);
+		int n = atoi(str);
+		if (n == 0)
+		{
+			for (int i = j; i == 0; i--)
+			{
+				printf("%d\n", array[i]);
+			}
+		}
+		else if (n == 100)
+		{
+
+		}
+		else if (n == -100)
+		{
+			array[j] = 0;
+				j--;
+		}
+		else if (n < 0)
+		{
+			array[j++] = n;
+			j++;
+		}
+		else if (n > 0)
+		{
+			n *= -1;
+			array[j++] = n;
+			j++;
+		}
+	}
+
+	return 0;
 }
 /*0: 現在のリストに格納されている全てのデータを「,」で区切って表示する
 100: リストの先頭のデータを削除する。削除する際に、データの値を出力する
