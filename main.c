b97dc6b07fa97372c48baf6244671c7548973ba2
Narita1910 <43438380+Narita1910@users.noreply.github.com>
2018-10-18 18:30:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,40 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[7];
+    char str[100];
+    int array[100];
+    int index=0,i;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        int n=atoi(str);
+        
+        if(1<=n&&n<100){
+        array[index]=n;
+        index++;
+        }
+        
+        else if(n==100){
+            printf("%d\n",array[index-1]);
+            index--;
+        }
+        
+        else if(n==-100){
+            printf("%d\n",array[0]);
+         for(i=0;i<index-1;i++){
+             array[i]=array[i+1];
+         }
+            index--;
+        }
+        
+        else if(n==0){
+            for(i=index-1;i>=0;i--){
+                printf("%d",array[i]);
+                if(i>0){
+                    printf(",");
+                }
+            }
+            printf("\n");
+        }
     }
 
     return 0;
