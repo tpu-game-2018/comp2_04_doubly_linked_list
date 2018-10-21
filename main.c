380024ded56256ef542d2d629c60418b4fac81b2
kireha <43433438+kireha@users.noreply.github.com>
2018-10-21 23:05:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,15 @@ int main(int argc, char *argv[])
 {
     char str[7];
     char a[100];
-    int i=0,j;
+    int i=0,j,x;
     while(fgets(str, sizeof(str), stdin)){
-        if(str)
+        x=atoi(str);
+        if(0<x&&x<100)
+            a[i]=x;
+        if(-100<x&&x<0)
+            a[i]=-x;
+        if(x==100)
+        if(x==-100)
     }
 
     return 0;
