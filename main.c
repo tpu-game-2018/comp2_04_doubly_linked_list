9f85cb18a3f7fd2e35eb96c74dab7bcaf10a47b2
kireha <43433438+kireha@users.noreply.github.com>
2018-10-21 22:54:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 int main(int argc, char *argv[]) 
 {
     char str[7];
-    
+    char a[100];
+    int i=0,j;
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        if(str)
     }
 
     return 0;
