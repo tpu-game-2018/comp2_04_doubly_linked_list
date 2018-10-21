14c07a731278efc1575f0273e99ef2931db1ec41
oonuki1030 <43439440+oonuki1030@users.noreply.github.com>
2018-10-21 20:49:43 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,65 @@
 
 int main(int argc, char *argv[]) 
 {
-    char str[7];
+    char str[100];
+    int n[100];
+    int i = 0;
+    int j;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        int in = out(str);
+         
+         
+         if(in > 0 && i == 0 && in < 100)
+ 
+         {
+ 
+             n[0] = in;
+ 
+             i++;
+ 
+         }
+         else if(in < 0 && i == 0 && in > -100)
+ 
+         {
+ 
+             n[0] = -in;
+ 
+             i++;
+ 
+         }
+ 
+         else if(in > 0 && i > 0 && in < 100)
+ 
+         {
+ 
+             for(j = i; j > 0; j--)
+ 
+             {
+ 
+                 n[j] = n[j - 1];
+ 
+             }
+ 
+             
+ 
+             n[0] = in;
+ 
+             i++;
+ 
+         }
+         
+        else if(in < 0 && i > 0 && in > -100)
+         {
+             for(j = i; j > 0; j--)
+             {
+                 n[j] = n[j - 1];
+             }
+             
+             n[0] = -input;
+            n[i] = -input;
+             i++;
+        }
+        
+        retuen 0;
     }
-
-    return 0;
-}
