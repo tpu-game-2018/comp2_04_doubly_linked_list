9be95307291382e52eadcc2f3b9003c4c9b41606
oonuki1030 <43439440+oonuki1030@users.noreply.github.com>
2018-10-21 21:08:29 +0900

    Update main.c

--- main.c
+++ main.c
@@ -62,5 +62,70 @@ int main(int argc, char *argv[])
              i++;
         }
         
-        return 0;
-    }
+    else if(in == 0)
+ 
+         {
+ 
+             for(j = 0; j < i; j++)
+ 
+             {
+ 
+                 printf("%d",n[j]);
+ 
+                 
+ 
+                 if(j < i - 1)
+ 
+                 {
+ 
+                     printf(",");
+ 
+                 }
+ 
+             }
+ 
+             printf("\n");
+ 
+         }
+ 
+         else if(in == 100)
+ 
+         {
+ 
+             printf("%d",n[0]);
+ 
+             
+ 
+             for(j = 0; j < i - 1; j++)
+ 
+             {
+ 
+                 n[j] = n[j + 1];
+ 
+             }
+ 
+             
+ 
+             i--;
+ 
+             printf("\n");
+ 
+         }
+         
+          else if(in == -100)
+ 
+         {
+ 
+             printf("%d",n[i -1]);
+             
+             n[i - 1] = 0;
+              
+             i--;
+ 
+             printf("\n"); 
+ 
+         }
+     }
+ 
+     return 0;
+ }
