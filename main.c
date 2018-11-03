d72c5584cb357ce7493362daba1aa33398792d88
Narita1910 <43438380+Narita1910@users.noreply.github.com>
2018-11-03 21:01:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,100 @@
 #include <stdio.h>
+#include<stdlib.h>
 
-int main(int argc, char *argv[]) 
+typedef int data_t;
+
+
+data_t stack_data[100];
+int index;
+int n;
+int push(data_t push_data)
 {
-    char str[100];
-    int array[100];
-    int index=0,i,tmp;
-    
-    while(fgets(str, sizeof(str), stdin)){
-        int n=atoi(str);
-        
-        if(1<=n&&n<100){
-        array[index]=n;
-        index++;
-        }
-        
-        if(-99<=n&&n<0){
-        array[index]=-n;
-         
-            tmp=array[index];
-        for(i=index;i>0;i--){
-   
-             array[i]=array[i-1];
-         }
-            array[0]=tmp;
-            index++;
-            
-        }
-        
-        else if(n==100){
-            printf("%d\n",array[index-1]);
-            index--;
-        }
-        
-        else if(n==-100){
-            printf("%d\n",array[0]);
-         for(i=0;i<index-1;i++){
-             array[i]=array[i+1];
-          }
-            index--;
-        }
-        
-        else if(n==0){
-            for(i=index-1;i>=0;i--){
-                printf("%d",array[i]);
-                if(i>0){
-                    printf(",");
-                }
-            }
-            printf("\n");
-        }
-    }
+	if (index < 100) {
+		stack_data[index] = push_data;
+		index++;
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+int pop1(data_t *pop_data)
+{
 
-    return 0;
- }
+	if (index > 0) {
+			index--;
+			*pop_data = stack_data[index];
+			return 1;
+	}
+	
+	else {
+		return 0;
+	}
+}
+
+int pop2(data_t *pop_data)
+{
+	int i;
+
+	if (index > 0) {
+				*pop_data = stack_data[0];
+				for (i = 0; i < index - 1; i++) {
+					stack_data[i] = stack_data[i + 1];
+				}
+				index--;
+				return 1;
+	}
+
+	else {
+		return 0;
+	}
+}
+
+void stackPrint()
+{
+	int i;
+
+	for (i = index - 1; i >= 0; i--) {
+		printf("%d", stack_data[i]);
+		if (i > 0) {
+			printf(",");
+		}
+	}
+	printf("\n");
+	
+}
+
+int main(int argc, char *argv[])
+{
+	char str[100];
+	int p;
+	index = 0;
+	while (fgets(str, sizeof(str), stdin)) {
+		n = atoi(str);
+
+		if (1 <= n && n <= 99) {
+			push(n);
+		}
+
+		if (n==100) {
+			pop1(&p);
+			printf("%d", p);
+			printf("\n");
+
+		}
+
+		if (n == -100) {
+			pop2(&p);
+			printf("%d", p);
+			printf("\n");
+		}
+
+		if (n == 0) {
+			stackPrint();
+		}
+
+
+	}
+
+	return 0;
+}
