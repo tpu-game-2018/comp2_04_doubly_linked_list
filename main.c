a337376f2897aebca8f93d03bca1c5a7c498eaf1
p156 <38420559+p156@users.noreply.github.com>
2018-11-06 02:32:07 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,51 +5,65 @@ int main(int argc, char *argv[])
 {
 	char str[7];
 	char array[100];
-	int index = 0;
+	int i = 0;
 	int j = 0;
 
 	while (fgets(str, sizeof(str), stdin)) {
-
-		printf("%s", str);
-		int n = atoi(str);
-		if (n == 0)
-		{
-			for (int i = j; i > 0; i--)
-			{
-				printf("%d\n", array[i]);
-				if(i > 1)
-				{
-					printf(",");
-				}
-			}
-		}
-		else if (n == 100)
-		{
-			for(int i = 1;i==j;i++)
-			{
-				array[i] = array[i+1];
-			}
-			j--;
-		}
-		else if (n == -100)
-		{
-			array[j] = 0;
-				j--;
-		}
-		else if (n < 0)
-		{
-			array[j++] = n;
-			j++;
-		}
-		else if (n > 0)
-		{
-			n *= -1;
-			array[j++] = n;
-			j++;
-		}
-	}
-
-	return 0;
+  		int n = atoi(str);
+		if(n > 0 && i == 0 && n < 100)
+        	{
+            		array[0] = n;
+            		i++;
+        	}
+        		else if(n < 0 && i == 0 && n > -100)
+        	{
+            		array[0] = -n;
+            		i++;
+        	}
+        	else if(n > 0 && i > 0 && n < 100)
+        	{
+            		for(j = i; j > 0; j--)
+            		{
+                		array[j] = array[j - 1];
+            		}
+            		array[0] = n;
+            		i++;
+        	}
+        	else if(n < 0 && i > 0 && n > -100)
+        	{
+            		array[i] = -n;
+            		i++;
+        	}
+        	else if(n == 0)
+       		{
+            		for(j = 0; j < i; j++)
+            		{
+                		printf("%d",array[j]);
+                		if(j < i - 1)
+                		{
+                    			printf(",");
+                		}
+            		}
+            		printf("\n");
+        	}
+        	else if(n == 100)
+        	{
+            		printf("%d",array[0]);
+            		for(j = 0; j < i - 1; j++)
+            		{
+                		array[j] = array[j + 1];
+            			i--;
+				printf("\n");
+        		}
+         	else if(n == -100)
+        	{
+            		printf("%d",array[i -1]);
+            		array[i - 1] = 0;
+            		i--;
+			printf("\n"); 
+  		}
+    	}
+    return 0;
 }
 /*0: 現在のリストに格納されている全てのデータを「,」で区切って表示する
 100: リストの先頭のデータを削除する。削除する際に、データの値を出力する
